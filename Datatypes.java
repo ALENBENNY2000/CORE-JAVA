b77b2f4cc1b9b5279a016cb37aaed40478341863
ALENBENNY2000 <alenbenny2000@gmail.com>
2023-12-16 22:41:58 +0530

    Introduction To Java

---
+++ Datatypes.java
@@ -0,0 +1,150 @@
+public class Datatypes{
+	public static void main(String args[]){
+
+byte shoeSize = 10;
+byte costOfPencils = 15;
+byte typesOMatterOfStates = 3;
+byte noOfFingers = 10;
+byte costOfErasers = 5;
+byte basicColorNumber = 12; 
+byte costOf1LBottle = 10;
+byte costOfNotebook = 100;
+byte noOfMinsInHr = 60;
+byte noOfDaysInWeek = 7;
+byte noOfMonthsInYear = 12;
+
+shoeSize = 8;
+costOfPencils = 12;
+costOfErasers = 7;
+costOf1LBottle = 12;
+costOfNotebook = 50;
+
+short noOfCountries = 195; 
+short noOfDaysInYear = 365;
+short costOfBag = 2500;
+short costOfShirt = 3000;
+short costOfWatch = 8000;
+short costOfChair = 500;
+short costOfDesk = 1000; 
+short noOfSecIn1Hr = 3600;
+short costOfBed = 2000;
+short costOfTableFan = 2650;
+short costOfWashingMachine = 10000;
+
+costOfBag = 2800;
+costOfWatch = 4000;
+costOfChair = 750;
+costOfDesk = 1300;
+costOfTableFan = 2880;
+
+int salaryOfFresher = 350000;
+int areaOfKerala = 38863;
+int costOfIphone15 = 76990;
+int roomRent = 35000; 
+int seatsInMelbourneStadium = 100024; 
+int costOfLaptop = 70000;
+int costOfTV = 150000;
+int costOfSplendor = 92000;
+int costOfAC = 23000;
+int costOfBicycle = 8500;
+int costOfSpeakers = 15000;
+
+roomRent = 40000;
+costOfLaptop = 76000;
+costOfTV = 130000;
+costOfAC = 20000;
+costOfSpeakers = 18000;
+
+long accountNo = 1235678465778L;
+long bankBalance = 234235245246l;
+long mobileNo = 9876543201l;
+long aadhaarNo = 234589671234l;
+long populationInKerala = 234235251L;
+long populatioInKanpur = 2397543L;
+long panNo = 3463575L;
+long areaOfUP = 243286L;
+long areaOfIndia = 3287263L;
+long costOfFerrariFXXK = 2600000l;
+long costOfBugatti = 192100000l;
+
+mobileNo = 6745238934l;
+aadhaarNo = 346756872389l;
+panNo = 2367458L;
+areaOfUP = 243287l;
+bankBalance = 345242624l;
+
+float costOfPen = 25.35f;
+float costOfEraser = 5.15f;
+float costOfScale = 10.5f;
+float costOfBook = 350.46f;
+float distance = 2354.60f;
+float speed = 235.5f;
+float marks = 50.50f;
+float electricityBill = 420.69f;
+float waterBill = 100.50f;
+float costOfKey = 20.5f;
+float costOfPencil = 13.5f;
+
+costOfEraser = 6.34f;
+distance = 7573495.56f;
+speed = 452.67f;
+marks = 89.90f;
+costOfKey = 25.8f;
+
+double bankBal = 42524622462.67;
+double salaryEmployer = 346242472.54;
+double ticketCharge = 3250.60;
+double carSpeed = 400.5;
+double distanceFromBanglrToKannur = 316.5; 
+double time = 22.5;
+double costOfBuilding = 323530.50;
+double costOfFurniture = 235478.80;
+double sqrtOfThree = 1.73205080757;
+double sqrtOfFive = 2.22360679775;
+double heightOfBurjKhalifa = 2716.5;
+
+bankBal = 23523136136.89;
+salaryEmployer = 2523652462.5;
+carSpeed = 43534.7;
+time = 18.4;
+ticketCharge = 5670.35;
+
+char alphabet = 'a';
+char symbol = '*';
+char plus = '+';
+char minus = '-';
+char gender = 'M';
+char number = '5';
+char romanNumber = 'X';
+char vowel = 'e';
+char divide = '/';
+char multiply = '*';
+char zero = '0';
+
+alphabet = 'f';
+symbol = '$';
+gender = 'F';
+number = '8';
+vowel = 'i';
+
+boolean isAllCatsWhite = false;
+boolean isThereClassToday = true;
+boolean is2GreaterThan4 = false;
+boolean isTodayWednesday = true;
+boolean areYouAnIndian = true;
+boolean isItRainingToday = true;
+boolean isItNightNow = true;
+boolean doYouWearSpecs = false;
+boolean isYourBirthYear2000 = true;
+boolean doYouLiveAbroad = false;
+boolean areYouSingleChild = false;
+
+isThereClassToday = false;
+isTodayWednesday = false;
+isItRainingToday = false;
+isItNightNow = false;
+doYouWearSpecs = true;
+
+System.out.println("Code Successfully Created");
+    }
+}
---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello
+{
+	public static void main(String args[])
+	{
+	double d = 34.3456;
+	System.out.println(d);
+	}
+}
