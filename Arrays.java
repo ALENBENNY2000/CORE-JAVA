4b5b3f3b58a7c6b724f06d6e8dff4b0f0a9e70d5
ALENBENNY2000 <alenbenny2000@gmail.com>
2023-12-16 22:35:01 +0530

    Arrays List

---
+++ Arrays.java
@@ -0,0 +1,62 @@
+public class Arrays {
+   public static void main(String[] args) {
+        
+        int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        char sym[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', '=', '/', '*', '-', '+'};
+        long add[] = {300,400,324,234,457467,3452,78657,23423,85857,25235213,43534,6756756,343456,36346,356356,885768,6756,45634,8567567,363454636};
+        double dot[] = {34.5343,456.324236524,435.345456,64564.45,6545.3465,345236.45345,346346.46745,3456345.64746,346346.5657,3456346.356356,345346.3456456456,23523.46456,34634.346346363,345.3456346,346.3463456,678.5474,234.546456,345.456,56.453,457.34523};
+        float var[] = {34.23f,56.234f,4564.23f,345345.54645f,54.23f,34.23f,56.234f,4564.23f,345345.54645f,54.23f,34.23f,56.234f,4564.23f,345345.54645f,54.23f,34.23f,56.234f,4564.23f,345345.54645f,54.23f,};
+        byte bits[] = {(byte)1, (byte)2, (byte)3, (byte)4, (byte)5,(byte)6, (byte)7, (byte)8, (byte)9, (byte)10, (byte)11, (byte)12, (byte)13, (byte)14, (byte)15, (byte)16, (byte)17, (byte)18, (byte)19, (byte)20};
+        short shot[] = {(short)1, (short)2, (short)3, (short)4, (short)5, (short)6, (short)7, (short)8, (short)9, (short)10, (short)11, (short)12, (short)13, (short)14, (short)15, (short)16, (short)17, (short)18, (short)19, (short)20};
+        boolean blen[] = {true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,};
+
+      for(int i = 0; i < 20; i++)
+      {
+         System.out.println(arr[i]);
+         System.out.println("---------------");
+      }
+      System.out.println("/------------------------------/");
+      for(int i = 0; i < 20; i++)
+      {
+         System.out.println(sym[i]);
+         System.out.println("---------------");
+      }
+      System.out.println("/------------------------------/");
+      for(int i = 0; i < 20; i++)
+      {
+         System.out.println(add[i]);
+         System.out.println("---------------");
+      }
+      System.out.println("/------------------------------/");
+      for(int i = 0; i < 20; i++)
+      {
+         System.out.println(dot[i]);
+         System.out.println("---------------");
+      }
+      System.out.println("/------------------------------/");
+      for(int i = 0; i < 20; i++)
+      {
+         System.out.println(var[i]);
+         System.out.println("---------------");
+      }
+      System.out.println("/------------------------------/");
+      for(int i = 0; i < 20; i++)
+      {
+         System.out.println(bits[i]);
+         System.out.println("---------------");
+      }
+      System.out.println("/------------------------------/");
+      for(int i = 0; i < 20; i++)
+      {
+         System.out.println(shot[i]);
+         System.out.println("---------------");
+      }
+      System.out.println("/------------------------------/");
+      for(int i = 0; i < 20; i++)
+      {
+         System.out.println(blen[i]);
+         System.out.println("---------------");
+      }
+      System.out.println("/------------------------------/");
+   }
+}
