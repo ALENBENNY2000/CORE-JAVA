a2a150ff143e593938101051013c32af85c29938
ALENBENNY2000 <alenbenny2000@gmail.com>
2023-12-16 22:43:13 +0530

    Method Types

---
+++ Methods.java
@@ -0,0 +1,38 @@
+public class Methods{
+	
+	public static void main(String args[]){
+
+	add();
+	subtract();
+	multiply();
+	divide();
+	modulus();
+
+}
+	static void add(){
+	System.out.println("Operator 1");
+	System.out.println("Operator 2");
+	System.out.println("Addition = Operator 1 + Operator 2");
+	}
+	static void subtract(){
+	System.out.println("Operator 1");
+	System.out.println("Operator 2");
+	System.out.println("Subtraction = Operator 1 - Operator 2");
+	}
+	static void multiply(){
+	System.out.println("Operator 1");
+	System.out.println("Operator 2");
+	System.out.println("Multiplication = Operator 1 * Operator 2");
+	}
+	static void divide(){
+	System.out.println("Divisor");
+	System.out.println("Divident");
+	System.out.println("Division = Divident / Divisor");
+	}
+	static void modulus(){
+	System.out.println("Operator 1");
+	System.out.println("Operator 2");
+	System.out.println("Modulus = Operator 1 % Operator 2");
+	}
+	
+}
